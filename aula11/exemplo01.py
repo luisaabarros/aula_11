a5e5612085105e846ecbe1198da3203349a7cebe
luisaabarros <luisaabarros1707@gmail.com>
2026-01-06 19:14:42 -0300

    gerar numeros decimais

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -1,6 +1,20 @@
 # Bibliotecas
 import random
 
-n = random.randint(1, 10)
-m = random.randint(1, 10)
-print(n, m)
+
+# random.randint --> numeros inteiros aleatórios
+
+# n = random.randint(1, 10)
+# m = random.randint(1, 10)
+# print(n, m)
+
+# ----------------------------------------------
+
+# Gerar numeros decimais
+# (o ':.2f' serve para limitar as casas decimais até 2)
+# o 'round' serve para arredondar os numeros
+
+n_decimal = random.uniform(1, 10)
+print(f'{n_decimal:.2f}')
+numero_decimal = round(n_decimal, 1)
+print(numero_decimal)
